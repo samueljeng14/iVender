f4c3e7c4d3a68f47ee38e44e01f52f555915a770
samueleng14 <samueljengmts@gmail.com>
2019-09-24 09:30:19 +0800

    initial

---
+++ index.js
@@ -0,0 +1,112 @@
+import tableData from "./tableData.js";
+import select from "./select.js";
+import inputForm from "./inputForm.js"
+
+
+var vm = new Vue({
+	el: "#app1",
+	data() {
+		return {
+			tableData: tableData,
+			select: select,
+			inputForm: inputForm,
+			showTable: true,
+			tableLoading: false,
+		
+			currentPage: 1, // 当前页码
+			total: '', // 总条数
+			pageSize: 10 // 每页的数据条数	
+		}
+	},
+
+	methods: {
+		show: function (event) {
+			this.showTable = true
+			this.tableLoading = false
+			setTimeout(() => this.tableLoading = false, 1000)
+			this.tableLoading = true
+		},
+		// 多選，輸入tableData(這個一定要)，key換成自己要的欄位
+		multipleSelect: function (tableData, key) {
+			return inputForm[key].length == 0 ? true : this.inputForm[key].some(el => tableData[key].includes(el))
+		},
+		// 滑條 slider，輸入tableData(這個一定要)，key換成自己要的欄位
+		range: function (tableData, key) {
+			return (tableData[key] >= this.inputForm[key].value[0] && tableData[key] <= this.inputForm[key].value[1])
+		},
+		// 給app使用時間的
+		rangeAppTime: function (tableData, key) {
+			return tableData[key] >= 12 ? true : (tableData[key] >= this.inputForm[key].value[0] && tableData[key] <= this.inputForm[key].value[1])
+		},
+		// 不用range，只有一個值的slider
+		slider: function (tableData, key) {
+			return (tableData[key] <= this.inputForm[key].value)
+		},
+		// 兩層級選擇器，只要用tableData就可以了，其他寫死
+		cascader: function (tableData) {
+			return inputForm.mostActPlace.length == 0 ? true : inputForm.mostActPlace.some(el =>
+				tableData.mostActPlace[0].includes(el[0]) &&
+				tableData.mostActPlace[1].includes(el[1])
+			)
+		},
+
+
+		handleSizeChange(val) {
+			this.currentPage = 1;
+			this.pageSize = val;
+		},
+		handleCurrentChange(val) {
+			this.currentPage = val;
+		}
+
+
+	},
+	computed: {
+		filtered: function () {
+			var filteredData = this.tableData.filter(tableData =>
+
+				this.multipleSelect(tableData, "sex") &&
+				this.range(tableData, "age") &&
+				this.multipleSelect(tableData, "jobType") &&
+				this.multipleSelect(tableData, "mostVehicle") &&
+				this.range(tableData, "avgWalkTime") &&
+				this.range(tableData, "avgMotoTime") &&
+				this.range(tableData, "avgBusTime") &&
+				this.range(tableData, "avgMetroTime") &&
+				this.range(tableData, "avgCarTime") &&
+				this.slider(tableData, "googleMapUpdate") &&
+				this.range(tableData, "useCash") &&
+				this.range(tableData, "useCreditCard") &&
+				this.range(tableData, "useEasyCard") &&
+				this.range(tableData, "useMobilePay") &&
+				this.multipleSelect(tableData, "mostPayWay") &&
+				this.range(tableData, "avgMonthCost") &&
+				this.multipleSelect(tableData, "mostBuyPlace") &&
+				this.slider(tableData, "invoiceVehicleUpdate") &&
+				this.multipleSelect(tableData, "avgGetupTime") &&
+				this.multipleSelect(tableData, "avgSleepTime") &&
+				this.multipleSelect(tableData, "avgOutTime") &&
+				this.multipleSelect(tableData, "avgHomeTime") &&
+				this.range(tableData, "avgHomeDuration") &&
+				this.range(tableData, "avgOutDuration") &&
+				this.slider(tableData, "lifeUpdate") &&
+				this.rangeAppTime(tableData, "sportAppUseTime") &&
+				this.rangeAppTime(tableData, "healthAppUseTime") &&
+				this.rangeAppTime(tableData, "shoppingAppUseTime") &&
+				this.rangeAppTime(tableData, "entertainmentAppUseTime") &&
+				this.rangeAppTime(tableData, "educationAppUseTime") &&
+				this.rangeAppTime(tableData, "photoAppUseTime") &&
+				this.rangeAppTime(tableData, "travelAppUseTime") &&
+				this.rangeAppTime(tableData, "socialAppUseTime") &&
+				this.rangeAppTime(tableData, "finaceAppUseTime") &&
+				this.cascader(tableData)
+			)
+			// var data30 = filteredData.slice(0, 30)
+			return filteredData
+		},
+		count: function () {
+			this.total = this.filtered.length
+			return this.filtered.length
+		}
+	}
+})
---
+++ inputForm.js
@@ -0,0 +1,222 @@
+export default {
+  sex: "",
+  age: {
+    value: [0, 100],
+    marks: {
+      0: "0歲",
+      20: "20歲",
+      40: "40歲",
+      60: "60歲",
+      80: "80歲",
+      100: "100歲"
+    }
+  },
+  jobType: "",
+  mostVehicle: "",
+
+  avgWalkTime: {
+    value: [0, 120],
+    marks: {
+      0: "0min",
+      30: "30min",
+      60: "60min",
+      90: "90min",
+      120: "120min"
+    }
+  },
+  avgMotoTime: {
+    value: [0, 120],
+    marks: {
+      0: "0min",
+      30: "30min",
+      60: "60min",
+      90: "90min",
+      120: "120min"
+    }
+  },
+  avgBusTime: {
+    value: [0, 120],
+    marks: {
+      0: "0min",
+      30: "30min",
+      60: "60min",
+      90: "90min",
+      120: "120min"
+    }
+  },
+  avgMetroTime: {
+    value: [0, 120],
+    marks: {
+      0: "0min",
+      30: "30min",
+      60: "60min",
+      90: "90min",
+      120: "120min"
+    }
+  },
+  avgCarTime: {
+    value: [0, 120],
+    marks: {
+      0: "0min",
+      30: "30min",
+      60: "60min",
+      90: "90min",
+      120: "120min"
+    }
+  },
+  mostActPlace: "",
+  googleMapUpdate: {
+    value: 8,
+    marks: {
+      1: "1周",
+      4: "1個月",
+      8: "2個月"
+    }
+  },
+
+  useCash: {
+    value: [0, 50],
+    marks: {
+      0: "0次",
+      50: "50次"
+    }
+  },
+  useCreditCard: {
+    value: [0, 50],
+    marks: {
+      0: "0次",
+      50: "50次"
+    }
+  },
+  useEasyCard: {
+    value: [0, 50],
+    marks: {
+      0: "0次",
+      50: "50次"
+    }
+  },
+  useMobilePay: {
+    value: [0, 50],
+    marks: {
+      0: "0次",
+      50: "50次"
+    }
+  },
+  mostPayWay: "",
+
+  avgMonthCost: {
+    value: [0, 200000],
+    marks: {
+      0: "0",
+      20000: "2萬",
+      50000: "5萬",
+      100000: "10萬",
+      200000: "20萬"
+    }
+  },
+  mostBuyPlace: "",
+  invoiceVehicleUpdate: {
+    value: 8,
+    marks: {
+      1: "1周",
+      4: "1個月",
+      8: "2個月"
+    }
+  },
+  avgGetupTime: "",
+  avgSleepTime: "",
+  avgOutTime: "",
+  avgHomeTime: "",
+  avgHomeDuration: {
+    value: [0, 24],
+    marks: {
+      0: "0hr",
+      24: "24hr"
+    }
+  },
+  avgOutDuration: {
+    value: [0, 24],
+    marks: {
+      0: "0hr",
+      24: "24hr"
+    }
+  },
+  lifeUpdate: {
+    value: 8,
+    marks: {
+      1: "1周",
+      4: "1個月",
+      8: "2個月"
+    }
+  },
+  mostUseApp: "",
+
+  sportAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  healthAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  shoppingAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  entertainmentAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  educationAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  photoAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  travelAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  socialAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  finaceAppUseTime: {
+    value: [0, 12],
+    marks: {
+      0: "0hr",
+      12: "12hr"
+    }
+  },
+  // 實驗用資料
+  moneySelector: [0, 100000],
+  ageSelector: [0, 100],
+  locationSelector: [],
+  getupSelector: ''
+}
